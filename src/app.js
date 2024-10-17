0eaa4b2ddd930f65f6de1a034787ccbfa696adcd
10ErickHerrera <10erickherrera@gmail.com>
2024-10-17 03:30:57 +0000

    BuenasPracticas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-  // array with the words
   let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
   let action = ["ate ", "peed ", "crushed ", "broke "];
   let what = ["my homework ", "my phone ", "the car "];
@@ -11,18 +10,16 @@ window.onload = function() {
     "while I was praying"
   ];
   function generateExcuse() {
-    // declaring random variables
-    let sentence1 = Math.floor(Math.random() * who.length);
-    let sentence2 = Math.floor(Math.random() * action.length);
-    let sentence3 = Math.floor(Math.random() * what.length);
-    let sentence4 = Math.floor(Math.random() * when.length);
+    let primeraSentencia = Math.floor(Math.random() * who.length);
+    let segundaSentencia = Math.floor(Math.random() * action.length);
+    let terceraSentencia = Math.floor(Math.random() * what.length);
+    let cuartaSentencia = Math.floor(Math.random() * when.length);
 
-    // creating a sentence (the excuse)
     document.querySelector("#excuse").innerHTML =
-      `<span class="who">${who[sentence1]}</span> ` +
-      `<span class="action">${action[sentence2]}</span> ` +
-      `<span class="what">${what[sentence3]}</span> ` +
-      `<span class="when">${when[sentence4]}</span>`;
+      `<span class="who">${who[primeraSentencia]}</span> ` +
+      `<span class="action">${action[segundaSentencia]}</span> ` +
+      `<span class="what">${what[terceraSentencia]}</span> ` +
+      `<span class="when">${when[cuartaSentencia]}</span>`;
   }
   generateExcuse();
   document.getElementById("generateButton").onclick = generateExcuse;
